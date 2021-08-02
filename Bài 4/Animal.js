7a725eb1f6e0015f3bfe4fb6c545c97782f96003
vanhopld98 <vanhopld98@gmail.com>
2021-08-02 10:59:06 +0700

    Module1

--- Bài 4/Animal.js
+++ Bài 4/Animal.js
@@ -27,10 +27,3 @@ class Animal {
         return document.write("Tên của Animal = " + this.name + "<br>" + " Cân nặng = " + this.weight);
     }
 }
-
-let objAnimal1 = new Animal();
-objAnimal1.setName("Elephant");
-objAnimal1.setWeight(45.6);
-objAnimal1.toString();
-let objAnimal2 = new Animal("Elephant1", 65.4);
-objAnimal2.setName("Mouse");
---
+++ Bài 4/main.js
@@ -0,0 +1,6 @@
+let objAnimal1 = new Animal();
+objAnimal1.setName("Elephant");
+objAnimal1.setWeight(45.6);
+objAnimal1.toString();
+let objAnimal2 = new Animal("Elephant1", 65.4);
+objAnimal2.setName("Mouse");
